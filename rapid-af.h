412cbc3a5dd5ebe868ad095c1ada052a169ad94f
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2021-02-23 10:27:43 +0100

    Add documentation for Options struct

--- rapid-af.h
+++ rapid-af.h
@@ -7,23 +7,23 @@ using namespace cv;
 
 namespace rapid_af {
 struct Options {
-    bool multiThreading = true;
-    uint padding = 100;
-    double agreementThreshold = 5;
+    bool multiThreading = true;      //!< Compute image preprocessing using multiple threads
+    uint padding = 100;              //!< Padding used for cross correlation
+    double agreementThreshold = 5;   //!< Maximum allowed discrepancy between displacements
 
-    bool bin_enable = true;
-    double bin_thresholdPercentage = 0.6;
+    bool bin_enable = true;                 //!< Enable binarize
+    double bin_thresholdPercentage = 0.6;   //!< Percentage of maximum for binarization
 
-    bool dog_enable = true;
-    int dog_ksize = 100;
-    double dog_sigma1 = 5;
-    double dog_sigma2 = 10;
+    bool dog_enable = true;  //!< Enable Difference-of-Gaussians (DoG)
+    int dog_ksize = 100;     //!< Kernel size for DoG
+    double dog_sigma1 = 5;   //!< Sigma of first Gaussian
+    double dog_sigma2 = 10;  //!< Sigma of second Gaussian
 
-    bool canny_enable = true;
-    int canny_ksize = 29;
-    double canny_sigma = 20;
-    double canny_alpha = 0;
-    double canny_beta = 1;
+    bool canny_enable = true;  //!< Enable Canny filter
+    int canny_ksize = 29;      //!< Kernel size for Canny filter
+    double canny_sigma = 20;   //!< Smoothing sigma for Canny filter
+    double canny_alpha = 0;    //!< Lower threshold for hysteresis thresholding
+    double canny_beta = 1;     //!< Higher threshold for hysteresis thresholding
 };
 
 Mat binarize(const Mat &image, double percentage);
